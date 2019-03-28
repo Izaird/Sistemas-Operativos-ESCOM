d47314466a1c8296d09da7ba229b169d410d810d
EdgarMung <lamborguini720@gmail.com>
2019-03-28 15:16:45 -0600

    Programa con comentarios intermedios

--- Practica_02/Practica2C.c
+++ Practica_02/Practica2C.c
@@ -5,22 +5,24 @@
 #include <sys/types.h>
 
 int main(){
-
+    //Creacion De Variables para Ingresar lo que regrese FORK()
     pid_t pid;
     pid_t PidHijos;
     pid_t PidHijos2;
     
+    //Impresion De los Datos del Proceso Padre Antes de Iniciar la Creacion de Hijos
     printf("Hola soy el padre\n");
     printf("Mi proceso padre es: %d\n",getppid());
     printf("Mi identificador es: %d\n\n",getpid());
     
-
+    //Inicio de la Creacion De Hijos
     pid = fork();
     if (pid == 0) {
+        //Datos Del Primer Hijo
         printf("\tHola soy el proceso hijo\n");
         printf("\tMi proceso padre es: %d\n",getppid());
         printf("\tMi identificador es: %d\n\n",getpid());
-      
+            //Creacion de los 3 Hijos del Primer Hijo
             for (int i = 0; i < 3; ++i)
             {   
                PidHijos = fork();
@@ -30,18 +32,19 @@ int main(){
                     printf("\t\tMi proceso padre es: %d\n",getppid());
                     printf("\t\tMi identificador es: %d\n\n",getpid());
 
+                    //Creacion de los 2 Hijos por cada Hijo Creado Anteriormente    
                     for (int i = 0; i < 2; ++i)
                     {   
-                        PidHijos = fork();
-                        if (PidHijos ==0)
+                        PidHijos2 = fork();
+                        if (PidHijos2 ==0)
                         {
                             printf("\t\t\tHola soy el proceso hijo del hijo, %d\n",i);
                             printf("\t\t\tMi proceso padre es: %d\n",getppid());
                             printf("\t\t\tMi identificador es: %d\n\n",getpid()); 
-                            break; 
+                            break;//Se utiliza un break para lograr que solo se creen los hijos necesarios 
                         }
                         else
-                            wait(NULL); 
+                            wait(NULL); //wai
                     } 
                     break; 
                 }

04fdc4e590a41abd7b018b621ab8b7dbb4738468
EdgarMung <lamborguini720@gmail.com>
2019-03-28 12:16:27 -0600

    Nueva actualizacion, hasta los hijos del tercer nivel

---
+++ Practica_02/Practica2C.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wait.h>
+#include <sys/types.h>
+
+int main(){
+
+    pid_t pid;
+    pid_t PidHijos;
+    
+    printf("Hola soy el padre\n");
+    printf("Mi proceso padre es: %d\n",getppid());
+    printf("Mi identificador es: %d\n\n",getpid());
+    
+
+    pid = fork();
+    if (pid == 0) {
+        printf("\tHola soy el proceso hijo\n");
+        printf("\tMi proceso padre es: %d\n",getppid());
+        printf("\tMi identificador es: %d\n\n",getpid());
+      
+            for (int i = 0; i < 2; ++i)
+            {
+               PidHijos = fork();
+                if (PidHijos ==0)
+                {
+                    printf("\t\tHola soy el proceso hijo del hijo, %d\n",i);
+                    printf("\t\tMi proceso padre es: %d\n",getppid());
+                    printf("\t\tMi identificador es: %d\n\n",getpid());  
+                }
+                else
+                    wait(NULL);
+            }
+        }
+    
+    else
+        wait(NULL);
+        
+    return 0;
+}
